aa49f2cd9e9d42376bd253e8a64bf144b8e7ab87
ezaki-shogo <71621496+ezaki-shogo@users.noreply.github.com>
2020-09-28 09:16:40 +0900

    ユーザーテーブルの作成

---
+++ db/migrate/20200928001525_add_nickname_to_users.rb
@@ -0,0 +1,7 @@
+class AddNicknameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :nickname, :string
+    add_column :users, :email, :string
+    add_column :users, :password, :string
+  end
+end
