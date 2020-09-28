a4e66997f1c469351f7307692f77d4b24afbe797
ezaki-shogo <71621496+ezaki-shogo@users.noreply.github.com>
2020-09-28 09:34:26 +0900

    デバイスに関するビューファイルの配置

--- db/migrate/20200928001525_add_nickname_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNicknameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :nickname, :string
-    add_column :users, :email, :string
-    add_column :users, :password, :string
-  end
-end
